beacdc484afb55ee3a4519e2e182268899c5b1c4
Vickistan <vicki@stanfield.net>
2013-09-27 14:47:38 -0400

    create_centos6.3.py

---
+++ create_centos6.3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import pyrax
+import sys
+
+sys.argv=""
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+if sys.argv == "":
+    server_name = raw_input("Enter a name for the server: ")
+    print ("You entered " + server_name)
+else:
+    server_name = sys.argv[1]
+
+creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
+pyrax.set_credential_file(creds_file)
+cs = pyrax.cloudservers
+#server_name = pyrax.utils.random_name(8)
+
+cent_image = [img for img in cs.images.list()
+        if "CentOS 6.3" in img.name][0]
+print "Centos Image:", cent_image
+
+flavor_512 = [flavor for flavor in cs.flavors.list()
+        if flavor.ram == 512][0]
+print "512 Flavor:", flavor_512
+        else flavor.ram == 1024][0]
+		print "1024 Flavor:", flavor_1024
+
+server = cs.servers.create(server_name, cent_image.id, flavor_512.id)
+print "Name:", server.name
+print "ID:", server.id
+print "Status:", server.status
+print "Admin Password:", server.adminPass
+print "Networks:", server.networks
